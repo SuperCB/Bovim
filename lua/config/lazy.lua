d16fe9eb8327f932bf84a23590c7b32a47c2d6b2
cuibo <1956699404@qq.com>
2023-09-06 18:37:32 +0800

    feat:change color

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.lang.cmake" },
+    { import = "lazyvim.plugins.extras.lang.python-semshi" },
     { import = "plugins" },
   },
   defaults = {
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,195 +1,194 @@
-return {{
+return {
+  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        light_style = "day", -- The theme is used when the background is set to light
-        transparent = true, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-        styles = {
-            -- Style to be applied to different syntax groups
-            -- Value is any valid attr-list value for `:help nvim_set_hl`
-            -- comments = { italic = true },
-            -- functions = {},
-            -- variables = {}
-            -- Background styles. Can be "dark", "transparent" or "normal"
-            -- sidebars = "dark", -- style for sidebars, see below
-            -- floats = "dark", -- style for floating windows
-        },
-        sidebars = {"qf", "help"}, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = false, -- dims inactive windows
-        lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      light_style = "day", -- The theme is used when the background is set to light
+      transparent = true, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        -- comments = { italic = true },
+        -- functions = {},
+        -- variables = {}
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        -- sidebars = "dark", -- style for sidebars, see below
+        -- floats = "dark", -- style for floating windows
+      },
+      sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+      dim_inactive = false, -- dims inactive windows
+      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
 
-        --- You can override specific color groups to use other groups or a hex color
-        --- function will be called with a ColorScheme table
-        ---@param colors ColorScheme
-        on_colors = function(colors)
-        end,
+      --- You can override specific color groups to use other groups or a hex color
+      --- function will be called with a ColorScheme table
+      ---@param colors ColorScheme
+      on_colors = function(colors) end,
 
-        --- You can override specific highlights to use other groups or a hex color
-        --- function will be called with a Highlights and ColorScheme table
+      --- You can override specific highlights to use other groups or a hex color
+      --- function will be called with a Highlights and ColorScheme table
 
-        ---@param highlights Highlights
-        ---@param colors ColorScheme
-        on_highlights = function(highlights, colors)
+      ---@param highlights Highlights
+      ---@param colors ColorScheme
+      on_highlights = function(highlights, colors)
+        highlights["LineNrAbove"] = {
+          fg = "#33a3dc",
+        }
+        highlights["LineNrBelow"] = {
+          fg = "#fcf16e",
+        }
 
-            highlights["LineNrAbove"] = {
-                fg = "#33a3dc"
-            }
-            highlights["LineNrBelow"] = {
-                fg = "#fcf16e"
-            }
+        highlights["@lsp.type.variable"] = {
+          fg = "#afdfe4",
+        }
+        highlights["@variable"] = {
+          fg = "#afdfe4",
+          style = {},
+        }
 
-            highlights["@lsp.type.variable"] = {
-                fg = "#afdfe4"
-            }
-            highlights["@variable"] = {
-                fg = "#afdfe4",
-                style = {}
-            }
-            highlights["@method"] = {
-                fg = "#f58220"
-            }
-            highlights["@lsp.type.function"] = {
-                fg = "#f58220"
-            }
-            highlights["@lsp.type.macro"] = {
-                fg = "#fab27b"
-            }
-            highlights.Comment = {
-                fg = "#007947"
+        highlights["@lsp.type.function"] = {
+          fg = "#f47920",
+          style = {
+            bold = true,
+          },
+        }
+        highlights["@lsp.type.macro"] = {
+          fg = "#fab27b",
+        }
+        highlights.Comment = {
+          fg = "#007947",
+        }
+        highlights["@lsp.type.comment"] = {
+          fg = "#007947",
+        }
+        highlights["@string.documentation"] = {
+          fg = "#1d953f",
+        }
+        highlights["@define"] = {
+          fg = "#00ae9d",
+        }
+        highlights["@include"] = {
+          fg = "#00ae9d",
+        }
+        highlights["@namespace"] = {
+          fg = "#ef4136",
+          style = {
+            bold = true,
+          },
+        }
+        highlights["@lsp.type.namespace"] = {
+          link = "@namespace",
+        }
+        highlights["@lsp.type.class"] = {
+          fg = "#FF4500",
+        }
 
-            }
-            highlights["@lsp.type.comment"] = {
-                fg = "#007947"
+        highlights["@type.builtin"] = {
+          fg = "#F08080",
+        }
 
-            }
-            highlights["@string.documentation"] = {
-                fg = "#1d953f"
-            }
-            highlights["@define"] = {
-                fg = "#00ae9d"
-            }
-            highlights["@include"] = {
-                fg = "#00ae9d"
-            }
-            highlights["@namespace"] = {
-                fg = "#ef4136",
-                style = {
-                    bold = true
-                }
-            }
-            highlights["@lsp.type.namespace"] = {
-                link = "@namespace"
-            }
-            highlights["@lsp.type.class"] = {
-               fg="#FF4500"
-            }
+        highlights["@lsp.type.namespace.go"] = {
+          fg = "#00ae9d",
+        }
+        highlights["@parameter.builtin"] = {
+          fg = "#ffe600",
+        }
+        highlights["@lsp.type.typeparameter"] = {
+          fg = "#d71345",
+          style = {
+            bold = true,
+          },
+        }
+        highlights["@lsp.type.parameter"] = {
+          fg = "#ffe600",
+        }
+        highlights["@lsp.type.type"] = {
+          fg = "#fab27b",
+        }
+        highlights["@lsp.typemod.type.defaultLibrary"] = {
+          fg = "#F08080",
+        }
+        highlights["@lsp.typemod.function.defaultLibrary"] = {
+          fg = "#00bfff",
+        }
+        highlights["@number"] = {
+          fg = "#fafad2",
+        }
+        highlights.Function = {
+          fg = "#f58220",
+          style = {
+            italic = false,
+          },
+        }
+        highlights.CursorLineNr = {
+          bg = "#ef5b9c",
+          fg = "#fffef9",
+        }
+        highlights.Folded = {
+          bg = "#8552a1",
+          fg = "#fcf16e",
+        }
+        highlights["@keyword"] = {
+          fg = "#FF99CC",
+          style = {
+            italic = false,
+          },
+        }
+        highlights.Keyword = {
+          fg = "#FF99CC",
 
-            highlights["@type.builtin"] = {
-               fg="#F08080"
-            }
+          style = {
+            italic = false,
+          },
+        }
+        highlights["@constructor"] = {
+          fg = "#FF6666",
 
-            highlights["@lsp.type.namespace.go"] = {
-                fg = "#00ae9d",
-            }
-            highlights["@parameter.builtin"] = {
-                fg = "#ffe600"
-            }
-            highlights["@lsp.type.typeparameter"] = {
-                fg = "#d71345",
-                style = {
-                    bold = true
-                }
-            }
-            highlights["@lsp.type.parameter"] = {
-                fg = "#ffe600"
+          style = {
+            italic = false,
+          },
+        }
+        highlights.WinSeparator = {
+          bold = true,
+          fg = "#ffd400",
+        }
 
-            }
-            highlights["@lsp.typemod.type.defaultLibrary"] = {
-                fg = "#F0E68C"
-            }
-            highlights["@lsp.typemod.function.defaultLibrary"] = {
-                fg = "#00bfff"
-            }
-            highlights["@number"] = {
-                fg = "#fafad2"
-            }
-            highlights.Function = {
-                fg = "#f58220",
-                style = {
-                    italic = false
-                }
-            }
-            highlights.CursorLineNr = {
-                bg = "#ef5b9c",
-                fg = "#fffef9"
+        -- Illuminate
+        highlights.illuminatedWord = {
 
-            }
-            highlights.Folded = {
-                bg = "#8552a1",
-                fg = "#fcf16e"
-            }
-            highlights["@keyword"] = {
-                fg = "#FF99CC",
-                style = {
-                    italic = false
-                }
-            }
-            highlights.Keyword = {
-                fg = "#FF99CC",
+          bg = "#fcf16e",
+          fg = "#8552a1",
+        }
 
-                style = {
-                    italic = false
-                }
-            }
-            highlights["@constructor"] = {
-                fg = "#FF6666",
-
-                style = {
-                    italic = false
-                }
-            }
-            highlights.WinSeparator = {
-                bold = true,
-                fg = "#ffd400"
-            }
-
-            -- Illuminate
-            highlights.illuminatedWord = {
-
-                bg = "#fcf16e",
-                fg = "#8552a1"
-            }
-
-            -- highlights.illuminatedCurWord = {
-            --    fg = "#8552a1",
-            --    bg = "#fcf16e",
-            -- }
-            highlights.IlluminatedWordText = {
-                fg = "#8552a1",
-                bg = "#fcf16e"
-
-            }
-            highlights.IlluminatedWordRead = {
-                fg = "#8552a1",
-                bg = "#fcf16e"
-            }
-            highlights.IlluminatedWordWrite = {
-                fg = "#8552a1",
-                bg = "#fcf16e"
-            }
-
-        end
-    }
-}, {
+        -- highlights.illuminatedCurWord = {
+        --    fg = "#8552a1",
+        --    bg = "#fcf16e",
+        -- }
+        highlights.IlluminatedWordText = {
+          fg = "#8552a1",
+          bg = "#fcf16e",
+        }
+        highlights.IlluminatedWordRead = {
+          fg = "#8552a1",
+          bg = "#fcf16e",
+        }
+        highlights.IlluminatedWordWrite = {
+          fg = "#8552a1",
+          bg = "#fcf16e",
+        }
+      end,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-        colorscheme = {"tokyonight"}
-    }
-}}
+      colorscheme = { "tokyonight" },
+    },
+  },
+}
